2529683efbbb1077409b40e7033d8df4ca50ca3c
DevMohi <morphex72@gmail.com>
2022-03-31 00:43:34 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Homepage from './components/Homepage/Homepage'
+import Coins from './components/Coins/Coins'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+import CoinDetails from './components/CoinDetails/CoinDetails'
+import BdAddress from './components/Contact/BdAddress'
+import UsAddress from './components/Contact/UsAddress'
+import NotFound from './components/NotFound/NotFound'
+
+const App = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Homepage></Homepage>}></Route>
+
+        <Route path='/coins' element={<Coins></Coins>}></Route>
+
+        <Route path='/coin-details/:id' element={<CoinDetails></CoinDetails>}></Route>
+
+        <Route path='/contact' element={<Contact></Contact>}>
+          {/* slash dewa lagbena auto inherit korbe  */}
+          <Route path='bd-address' element={<BdAddress></BdAddress>}></Route>
+          <Route path='us-address' element={<UsAddress></UsAddress>}></Route>
+        </Route>
+
+        <Route path='/about' element={<About></About>}></Route>
+
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
+      </Routes>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default App
